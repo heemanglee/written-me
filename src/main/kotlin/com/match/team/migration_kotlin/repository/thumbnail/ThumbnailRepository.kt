ccc5ff19ed6c211a1f3fa15edec7a80ce888d634
w1shope <wishhopelee@gmail.com>
2024-07-18 18:10:21 +0900

    chore: 불필요한 클래스 삭제

--- src/main/kotlin/com/match/team/migration_kotlin/repository/thumbnail/ThumbnailRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.match.team.migration_kotlin.repository.thumbnail
-
-import com.match.team.migration_kotlin.domain.thumbnail.Thumbnail
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ThumbnailRepository : JpaRepository<Thumbnail, Long> {
-
-}
